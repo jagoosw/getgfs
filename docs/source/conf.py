4de94b58a7edf7fb6a7e6cf8f7203ad763bcf46c
Jago Strong-Wright <jagoosw@protonmail.com>
2021-02-28 20:35:47 +0000

    Tidied

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,17 +13,18 @@
 import os
 import sys
 import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.split(os.path.abspath(".."))[0])
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'getgfs'
-copyright = '2021, Jago Strong-Wright'
-author = 'Jago Strong-Wright'
+project = "getgfs"
+copyright = "2021, Jago Strong-Wright"
+author = "Jago Strong-Wright"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,11 +32,10 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -48,11 +48,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
--- setup.py
+++ setup.py
@@ -3,26 +3,19 @@ from distutils.core import setup
 setup(
     # Application name:
     name="getgfs",
-
     # Version number (initial):
     version="0.0.0",
-
     # Application author details:
     author="Jago Strong-Wright",
     author_email="jagoosw@protonmail.com",
-
     # Packages
     packages=["getgfs"],
-
     # Details
     url="https://github.com/jagoosw/getgfs/",
-
     #
     license="LICENSE.txt",
     description="Extracts weather forecast variables from the NOAA GFS forecast in a pure python, no obscure dependencies way",
-
     long_description="""Please see the homepage as the readme wouldn't render here""",
-
     # Dependent packages (distributions)
     install_requires=[
         "scipy",
@@ -30,13 +23,12 @@ setup(
         "fuzzywuzzy",
         "numpy",
         "python_dateutil",
-        "regex"
+        "regex",
     ],
-
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Topic :: Text Processing :: Markup :: Markdown"
+        "Topic :: Text Processing :: Markup :: Markdown",
     ],
 )
